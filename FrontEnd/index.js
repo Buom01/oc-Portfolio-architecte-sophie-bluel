2c9bc7e8470b2e451f4da3b9c790df9ff1f01878
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-04-15 22:14:16 +0200

    Also load categories in parallel

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,10 +1,24 @@
-const gallery = document.getElementById('gallery');
+// Containers
+
+const gallery = document.getElementById('portfolio-gallery');
+const filters = document.getElementById('portfolio-filters');
+
+
+// Getters
 
 async function getWorks()
 {
     let res = await fetch("http://localhost:5678/api/works");
     return await res.json();
 };
+async function getCategories()
+{
+    let res = await fetch("http://localhost:5678/api/categories");
+    return await res.json();
+};
+
+
+// Renderers
 
 function createWorkElement({id, title, imageUrl, alt, categoryId})
 {
@@ -24,7 +38,33 @@ function createWorkElement({id, title, imageUrl, alt, categoryId})
     return root;
 }
 
-(async () => {
+function createCategoryElement({id, name})
+{
+    let root = document.createElement("button");
+
+    root.setAttribute('data-id', id);
+    root.innerText = name;
+
+    return root;
+}
+
+
+// Inits
+
+async function initWorks()
+{
     let works = await getWorks();
     gallery.replaceChildren(...works.map(createWorkElement));
+};
+
+async function initCategories()
+{
+    let categories = await getCategories();
+    filters.append(...categories.map(createCategoryElement));
+};
+
+// Main
+
+(async () => {
+    await Promise.all([initWorks(), initCategories()]);
 })();
